228c01d3991bc6d8e500190f879482b04fa9f6b6
Rohit Kalubarme <67742127+Rohit1802@users.noreply.github.com>
2022-12-13 22:14:50 +0530

    Update Hero.js

--- src/Hero.js
+++ src/Hero.js
@@ -10,7 +10,7 @@ function Hero() {
   return (
      <section className='hero'>
         <img src={Gallery} className='hero--photo' alt='Gallery'/>
-        <h1 className='hero--header'>Online Experiences</h1>
+        <h1 className='hero--header'>Online Experience</h1>
         <p className='hero--text'>Join unique interactive activities led by
              one-of-a-kind hosts-all without leaving home.</p>
      </section>
